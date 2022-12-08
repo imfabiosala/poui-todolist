305e1af8ac31be625e8551a2b5306825be9ebb42
Fábio Sala <imfabiosala@gmail.com>
2022-12-08 13:13:23 -0300

    [FEAT] Created and config. add. method to tasks.

--- src/app/tasks/add/add.component.ts
+++ src/app/tasks/add/add.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { Task, TaskService } from '../shared';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
+
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTask', { static:true }) formTask: NgForm
 
-  ngOnInit(): void {
+  task: Task;
+
+  constructor(private taskService: TaskService, private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.task = new Task(0, "", false);
+  }
+
+  add():void {
+    if (this.formTask.form.valid) {
+      this.taskService.add(this.task);
+      this.router.navigate(['/tasks'])
+    }
   }
 
 }
