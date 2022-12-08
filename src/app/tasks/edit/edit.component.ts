acc7272aff57727cce378fdce99075904b3e492c
Fábio Sala <imfabiosala@gmail.com>
2022-12-08 19:58:32 -0300

    [FEAT] Created edit methods and HTML interface.

--- src/app/tasks/edit/edit.component.ts
+++ src/app/tasks/edit/edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { Task, TaskService } from '../shared';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
+
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTask', { static:true }) formTask: NgForm;
+
+  task: Task;
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.params['id'];
+    this.task = this.taskService.findById(id);
   }
 
+  update(): void {
+    if (this.formTask.form.valid) {
+      this.taskService.update(this.task);
+      this.router.navigate(['/tasks']);
+    }
+  }
 }
